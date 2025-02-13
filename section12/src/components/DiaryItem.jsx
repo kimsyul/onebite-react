baea6023ca053e7692eaa6834a9a805878a5c4f7
kimsyul <yul_the@naver.com>
2025-02-13 18:11:14 +0900

    day20

--- section12/src/components/DiaryItem.jsx
+++ section12/src/components/DiaryItem.jsx
@@ -1,20 +1,26 @@
 import './DiaryItem.css';
-import { getEmotionImage } from '../util/get-emotion-imgage';
+import { getEmotionImage } from '../util/get-emotion-image';
 import Button from './Button';
 import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
 
 const DiaryItem = ({ id, emotionId, createdDate, content }) => {
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+
   return (
     <div className="DiaryItem">
-      <div className={`img_section img_section_${emotionId}`}>
+      <div onClick={() => nav(`/diary/${id}`)} className={`img_section img_section_${emotionId}`}>
         <img src={getEmotionImage(emotionId)} />
       </div>
-      <div className="info_section">
+      <div onClick={() => nav(`/diary/${id}`)} className="info_section">
         <div className="created_date">{new Date(createdDate).toLocaleDateString()}</div>
         <div className="content">{content}</div>
       </div>
       <div className="button_section">
-        <Button text={'수정하기'} />
+        <Button onClick={() => nav(`/edit/${id}`)} text={'수정하기'} />
+        <Button onClick={() => onDelete(id)} type={'NEGATIVE'} text={'삭제하기'} />
       </div>
     </div>
   );
