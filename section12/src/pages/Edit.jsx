a336709b470c81672a3fe8ef7d49ff0edfcdff22
Kim Seo-Yul <yul_the@naver.com>
2025-02-14 16:23:02 +0900

    day21

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,8 +1,48 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
+import { useContext } from 'react';
+import { DiaryDispatchContext, DiaryStateContext } from '../App';
 
 const Edit = () => {
   const params = useParams();
-  return <div>{params.id}번 일기입니다</div>;
+  const nav = useNavigate();
+  const { onDelete } = useContext(DiaryDispatchContext);
+  const data = useContext(DiaryStateContext);
+
+  const onClickDelete = () => {
+    if (window.confirm('일기를 정말 삭제할까요? 다시 복구되지 않아요!')) {
+      // 일기 삭제 로직
+      onDelete(params.id);
+      nav('/', { replace: true });
+    }
+  };
+
+  const getCurrentDiaryItem = () => {
+    const currentDiaryItem = data.find((item) => String(item.id) === String(params.id));
+
+    if (!currentDiaryItem) {
+      window.alert('존재하지 않는 일기입니다.');
+      nav('/', { replace: true });
+    }
+
+    return currentDiaryItem;
+  };
+
+  const currentDiaryItem = getCurrentDiaryItem();
+  console.log(currentDiaryItem);
+
+  return (
+    <div>
+      <Header
+        title={'일기 수정하기'}
+        leftChild={<Button onClick={() => nav(-1)} text={'< 뒤로 가기'} />}
+        rightChild={<Button onClick={onClickDelete} text={'삭제하기'} type={'NEGATIVE'} />}
+      />
+      <Editor />
+    </div>
+  );
 };
 
 export default Edit;
