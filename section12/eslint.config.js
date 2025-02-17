d290027b451d5b9d71001589fac3844560cb7bbe
Kim Seo-Yul <yul_the@naver.com>
2025-02-17 14:31:26 +0900

    day22

--- section12/eslint.config.js
+++ section12/eslint.config.js
@@ -1,8 +1,8 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -22,6 +22,7 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier,
     },
     rules: {
       ...js.configs.recommended.rules,
@@ -29,10 +30,11 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'no-unused-vars': 'warn',
+      'no-undef': 'error',
+      'prettier/prettier': 'warn', // 🔥 Prettier 규칙 적용
     },
   },
-]
+  configPrettier,
+];
--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -3,13 +3,14 @@ import Header from '../components/Header';
 import Button from '../components/Button';
 import Editor from '../components/Editor';
 import { useContext } from 'react';
-import { DiaryDispatchContext, DiaryStateContext } from '../App';
+import { DiaryDispatchContext } from '../App';
+import useDiary from '../hooks/useDiary';
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
-  const { onDelete } = useContext(DiaryDispatchContext);
-  const data = useContext(DiaryStateContext);
+  const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
+  const curDiaryItem = useDiary(params.id);
 
   const onClickDelete = () => {
     if (window.confirm('일기를 정말 삭제할까요? 다시 복구되지 않아요!')) {
@@ -19,19 +20,12 @@ const Edit = () => {
     }
   };
 
-  const getCurrentDiaryItem = () => {
-    const currentDiaryItem = data.find((item) => String(item.id) === String(params.id));
-
-    if (!currentDiaryItem) {
-      window.alert('존재하지 않는 일기입니다.');
+  const onSubmit = (input) => {
+    if (window.confirm('일기를 정말 수정할까요?')) {
+      onUpdate(params.id, input.createdDate.getTime(), input.emotionId, input.content);
       nav('/', { replace: true });
     }
-
-    return currentDiaryItem;
   };
-
-  const currentDiaryItem = getCurrentDiaryItem();
-  console.log(currentDiaryItem);
 
   return (
     <div>
@@ -40,7 +34,7 @@ const Edit = () => {
         leftChild={<Button onClick={() => nav(-1)} text={'< 뒤로 가기'} />}
         rightChild={<Button onClick={onClickDelete} text={'삭제하기'} type={'NEGATIVE'} />}
       />
-      <Editor />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
